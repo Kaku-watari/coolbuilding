6ca547b34bdfd1527e0531340e5c2b619e33125c
Kaku-Ishiwatari <44764936+Kaku-Ishiwatari@users.noreply.github.com>
2020-02-29 23:37:30 +0900

    text&URL

--- config.js
+++ config.js
@@ -31,7 +31,7 @@ module.exports = {
     {
       icon: 'fa-envelope-o',
       name: 'Email',
-      url: 'mailto:info@bhb-harry.netlify.com',
+      url: 'mailto:info@bhb-harry.co.jp',
     },
   ],
 };
